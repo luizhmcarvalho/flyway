e4fa2e302fc994ce37b985be32238449090b0499
Luiz Carvalho <luiz@nexo.ai>
2019-07-05 01:09:01 -0300

    I

---
+++ src/main/java/com/example/flywaydemo/api/UserController.java
@@ -0,0 +1,26 @@
+package com.example.flywaydemo.api;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.flywaydemo.domain.User;
+import com.example.flywaydemo.repository.IUserRepository;
+
+
+@RestController
+public class UserController {
+
+	private IUserRepository repository;
+	
+	UserController(IUserRepository userRepository){
+		repository = userRepository;
+	}
+	
+	@RequestMapping("/users")
+	public Iterable<User> getUsers(){
+		return repository.findAll();
+	}
+	
+}
---
+++ src/main/java/com/example/flywaydemo/repository/IUserRepository.java
@@ -0,0 +1,11 @@
+package com.example.flywaydemo.repository;
+
+import java.util.List;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import com.example.flywaydemo.domain.User;
+
+public interface IUserRepository extends PagingAndSortingRepository<User, Long> {
+
+
+}
